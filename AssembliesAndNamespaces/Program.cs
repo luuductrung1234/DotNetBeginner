d3d8f8b6a711a38e9d6b1f19a8654b037dff51bf
Luu Duc Trung <luuductrung1234@gmail.com>
2020-07-23 12:33:46 +0700

    reference to published package

---
+++ AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using SharedLibrary;
+using static System.Console;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter a color value in hex: ");
+            string hex = ReadLine();
+            WriteLine("Is {0} a valid color value? {1}",
+            arg0: hex, arg1: hex.IsValidHex());
+            Write("Enter a XML tag: ");
+            string xmlTag = ReadLine();
+            WriteLine("Is {0} a valid XML tag? {1}",
+            arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+            Write("Enter a password: ");
+            string password = ReadLine();
+            WriteLine("Is {0} a valid password? {1}",
+            arg0: password, arg1: password.IsValidPassword());
+        }
+    }
+}
