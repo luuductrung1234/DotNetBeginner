54838248e62aa50119bae007f09821617e9f451a
Thomas Luu (Tom) <thomas.luu@transwap.com>
2020-09-20 18:13:33 +0700

    define reusable Razor page

---
+++ PracticalApps/NorthwindEmployees/Areas/MyFeature/Pages/employees.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages; // PageModel
+using System.Linq;                         // ToArray()
+using System.Collections.Generic;          // IEnumerable<T>
+using PracticalApps.NorthwindContextLib;
+using PracticalApps.NorthwindEntitiesLib;
+
+namespace PracticalApps.MyFeatures.Pages
+{
+    public class EmployeesPageModel : PageModel
+    {
+        private NorthwindContext db;
+
+        public EmployeesPageModel(NorthwindContext injectedContext)
+        {
+            db = injectedContext;
+        }
+
+        public IEnumerable<Employee> Employees { get; set; }
+
+        public void OnGet()
+        {
+            Employees = db.Employees.ToArray();
+        }
+    }
+}
